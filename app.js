7a83d77e61c347e156663915609e9b40786a7b9a
calidevelops <176517277+calidevelops@users.noreply.github.com>
2025-03-11 18:15:56 -0400

    feat: implementar y llamar funcion para actualizar la lista de amigos.

--- app.js
+++ app.js
@@ -10,8 +10,23 @@ function agregarAmigo() {
         lista_amigos.push(nombre);
         entrada_nombre.value = '';
         console.log(lista_amigos);
+        actualizarListaAmigos();
 
     } else {
         alert('Por favor, inserte un nombre.');
+    }
+}
+
+
+const lista_amigos_elemento = document.querySelector('.name-list');
+
+//funcion para actualizar lista de amigos
+function actualizarListaAmigos() {
+    lista_amigos_elemento.innerHTML = "";
+
+    for (let i = 0; i < lista_amigos.length; i++) {
+        const amigo_elemento = document.createElement('li');
+        amigo_elemento.textContent = lista_amigos[i];
+        lista_amigos_elemento.appendChild(amigo_elemento);
     }
 }
